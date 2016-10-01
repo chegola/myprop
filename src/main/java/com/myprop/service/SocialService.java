131490835673b4ffba7451de0565fa75b7b258f2
chegola <arkhomk@yahoo.com>
2016-10-01 04:51:07 +0100

    get back email

--- src/main/java/com/myprop/service/SocialService.java
+++ src/main/java/com/myprop/service/SocialService.java
@@ -59,7 +59,7 @@ public class SocialService {
         String providerId = connection.getKey().getProviderId();
         User user = createUserIfNotExist(userProfile, langKey, providerId);
         createSocialConnection(user.getLogin(), connection);
-        //mailService.sendSocialRegistrationValidationEmail(user, providerId);
+        mailService.sendSocialRegistrationValidationEmail(user, providerId);
     }
 
     private User createUserIfNotExist(UserProfile userProfile, String langKey, String providerId) {
