0bf195af3e61131fcfe98cd01c63389e336d8612
chegola <arkhomk@yahoo.com>
2016-11-07 22:45:01 +0700

    Signed-off-by: chegola <arkhomk@yahoo.com>

--- src/main/java/com/myprop/service/impl/AnnouncementServiceImpl.java
+++ src/main/java/com/myprop/service/impl/AnnouncementServiceImpl.java
@@ -1,5 +1,9 @@
 package com.myprop.service.impl;
 
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.myprop.service.AnnouncementService;
 import com.myprop.domain.Announcement;
 import com.myprop.repository.AnnouncementRepository;
@@ -9,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+
+import retrofit2.Response;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -21,32 +27,68 @@ import java.util.List;
 public class AnnouncementServiceImpl implements AnnouncementService{
 
     private final Logger log = LoggerFactory.getLogger(AnnouncementServiceImpl.class);
-    
+
     @Inject
     private AnnouncementRepository announcementRepository;
-    
+
     /**
      * Save a announcement.
-     * 
+     *
      * @param announcement the entity to save
      * @return the persisted entity
      */
     public Announcement save(Announcement announcement) {
         log.debug("Request to save Announcement : {}", announcement);
         Announcement result = announcementRepository.save(announcement);
+
+        try {
+            this.pushTextMessage(result.getSubject());
+        } catch (Exception e) {
+            log.debug("push message to LINE" + e.getMessage());
+        }
+
+
         return result;
     }
 
+
+
+    private void pushTextMessage(String text) throws Exception{
+        log.debug("send text message:");
+        TextMessage textMessage = new TextMessage(text);
+        PushMessage pushMessage = new PushMessage("C7a6e87b46b03a61531e574979d97b3ab", textMessage);
+
+        Response<BotApiResponse> response = LineMessagingServiceBuilder
+                .create("FkQLYskmNpPlm6VMwwcaJW8/zvajBTgx/dTkl0REcOVG6P/VhTA0refze5F2lFFbRz+UpghUZNmEa52ufewHoS8dNBQ3Ij2lu+yImbkHWSsBstKH0jlRIWXmkNWkchv9Wh2TgTgff77UGkWXG7U3WAdB04t89/1O/w1cDnyilFU=")
+                .build()
+                .pushMessage(pushMessage)
+                .execute();
+        log.debug(response.code() + " " + response.message());
+    }
+
+
+    /*
+    @EventMapping
+    private void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+        log.debug("event: " + event);
+        new TextMessage()
+        new ReplyMessage(event.getReplyToken(), singletonList())
+        final BotApiResponse apiResponse = lineMessageService
+            .replyMessage(new ReplyMessage(event.getReplyToken(),
+                singletonList(new TextMessage(event.getMessage().getText()))))
+            .execute().body();
+    }
+    */
     /**
      *  Get all the announcements.
-     *  
+     *
      *  @param pageable the pagination information
      *  @return the list of entities
      */
-    @Transactional(readOnly = true) 
+    @Transactional(readOnly = true)
     public Page<Announcement> findAll(Pageable pageable) {
         log.debug("Request to get all Announcements");
-        Page<Announcement> result = announcementRepository.findAll(pageable); 
+        Page<Announcement> result = announcementRepository.findAll(pageable);
         return result;
     }
 
@@ -56,7 +98,7 @@ public class AnnouncementServiceImpl implements AnnouncementService{
      *  @param id the id of the entity
      *  @return the entity
      */
-    @Transactional(readOnly = true) 
+    @Transactional(readOnly = true)
     public Announcement findOne(Long id) {
         log.debug("Request to get Announcement : {}", id);
         Announcement announcement = announcementRepository.findOne(id);
@@ -65,7 +107,7 @@ public class AnnouncementServiceImpl implements AnnouncementService{
 
     /**
      *  Delete the  announcement by id.
-     *  
+     *
      *  @param id the id of the entity
      */
     public void delete(Long id) {
---
+++ src/main/java/com/myprop/service/line/LineWebhookObject.java
@@ -0,0 +1,65 @@
+package com.myprop.service.line;
+
+import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.model.event.FollowEvent;
+import com.linecorp.bot.model.event.JoinEvent;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.UnfollowEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * Created by che on 7/11/2559.
+ */
+@LineMessageHandler
+public class LineWebhookObject {
+
+    private final Logger log = LoggerFactory.getLogger(LineWebhookObject.class);
+
+    @Inject
+    private LineMessagingService lineMessagingService;
+
+    @EventMapping
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException {
+        TextMessageContent message = event.getMessage();
+    }
+
+    @EventMapping
+    public void handleUnfollowEvent(UnfollowEvent event) {
+        log.info("unfollowed this bot: {}", event);
+    }
+
+    @EventMapping
+    public void handleFollowEvent(FollowEvent event) {
+        String replyToken = event.getReplyToken();
+        this.replyText(replyToken, "Got followed event");
+    }
+
+    @EventMapping
+    public void handleJoinEvent(JoinEvent event) {
+        String replyToken = event.getReplyToken();
+        this.replyText(replyToken, "Joined " + event.getSource());
+    }
+
+    private void replyText(@NonNull String replyToken, @NonNull String message) {
+        if (replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken must not be empty");
+        }
+        if (message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, Collections.singletonList(message));
+    }
+
+}
